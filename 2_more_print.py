22f7ddcb46207b30ca5109ad7e544e5457268806
yzk <1500683092@qq.com>
2026-02-03 16:00:03 +0800

    first commit

---
+++ 2_more_print.py
@@ -0,0 +1,6 @@
+print("hello" + " world")
+print("let\'go")
+print("你好\n我是第二行")
+print("""静夜思
+床前看月光，疑是地上霜。
+举头望山月，低头思故乡。""")
---
+++ 3_variables.py
@@ -0,0 +1,7 @@
+greet = "你好，吃了么 "
+greet_chinese = greet
+greet_english = "hi, what\'s up "
+greet = greet_english
+print(greet + "张三")
+print(greet + "李四")
+print(greet_chinese + "李四")
